ae1556c5847f7b67d18a90a50e47b056937a1bdb
Felipe Sánchez <vegafelipe015@gmail.com>
2020-05-18 13:45:53 -0500

    Bar graph ready but...
    Still need to improve

---
+++ code/gas-impact-analyzer-in-crops/BarGraphic/BarGraph.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace BarGraphic
+{
+    partial class BarGraph
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.citiesComboBox = new System.Windows.Forms.ComboBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.yearComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Location = new System.Drawing.Point(12, 12);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(782, 347);
+            this.cartesianChart1.TabIndex = 0;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // citiesComboBox
+            // 
+            this.citiesComboBox.FormattingEnabled = true;
+            this.citiesComboBox.Items.AddRange(new object[] {
+            "BUENAVENTURA",
+            "GUADALAJARA DE BUGA",
+            "CALI",
+            "CANDELARIA",
+            "JAMUNDÍ",
+            "PALMIRA",
+            "YUMBO"});
+            this.citiesComboBox.Location = new System.Drawing.Point(101, 393);
+            this.citiesComboBox.Name = "citiesComboBox";
+            this.citiesComboBox.Size = new System.Drawing.Size(121, 21);
+            this.citiesComboBox.TabIndex = 1;
+            this.citiesComboBox.Text = "City";
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(434, 393);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 3;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // yearComboBox
+            // 
+            this.yearComboBox.FormattingEnabled = true;
+            this.yearComboBox.Items.AddRange(new object[] {
+            "2011",
+            "2012",
+            "2013",
+            "2014",
+            "2015",
+            "2016",
+            "2017"});
+            this.yearComboBox.Location = new System.Drawing.Point(270, 393);
+            this.yearComboBox.Name = "yearComboBox";
+            this.yearComboBox.Size = new System.Drawing.Size(121, 21);
+            this.yearComboBox.TabIndex = 2;
+            this.yearComboBox.Text = "Year";
+            // 
+            // BarGraph
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.yearComboBox);
+            this.Controls.Add(this.citiesComboBox);
+            this.Controls.Add(this.cartesianChart1);
+            this.Name = "BarGraph";
+            this.Text = "BarGraphic";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private System.Windows.Forms.ComboBox citiesComboBox;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.ComboBox yearComboBox;
+    }
+}
+
---
+++ code/gas-impact-analyzer-in-crops/BarGraphic/BarGraph.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using LiveCharts;
+using LiveCharts.WinForms;
+using LiveCharts.Wpf;
+using model;
+
+namespace BarGraphic
+{
+    public partial class BarGraph : Form
+    {
+        private double CO;
+        private double O3;
+        private double NO2;
+        private double SO2;
+        private double PM10;
+        private double PM25;
+        private DataManager dataManager;
+
+        public BarGraph()
+        {
+            InitializeComponent();
+            CO = 0;
+            O3 = 0;
+            NO2 = 0;
+            SO2 = 0;
+            PM10 = 0;
+            PM25 = 0;
+            dataManager = new DataManager();
+
+            cartesianChart1.AxisX = new AxesCollection
+            {
+                new Axis {
+                Title = "Pollutants",
+                Labels = new[]{"CO", "O3", "NO2", "SO2", "PM10", "PM2.5" },
+                LabelFormatter = value => value.ToString("N")
+
+                },
+            };
+
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            getAverageConcentration();
+
+            cartesianChart1.Series.Clear();
+
+            SeriesCollection sc = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = yearComboBox.Text,
+                    Values = new ChartValues<double> { CO, O3, NO2, SO2, PM10, PM25 },
+                    
+                    
+                },
+
+               
+            };
+
+           
+
+            cartesianChart1.Series = sc;
+
+        }
+
+        private void getAverageConcentration() 
+        {
+
+            dataManager.Measurements.Clear();
+
+            Dictionary<string, string> dictionay = new Dictionary<string, string>();
+
+            dictionay.Add("Nombre del municipio", citiesComboBox.Text);
+            dictionay.Add("Variable", "CO");
+
+            dataManager.filterDataForAir(dictionay);
+            double sum = 0;
+            int cont = 0;
+
+            foreach(Measurement m in dataManager.Measurements)
+            {
+
+                if (m.Date.Contains(yearComboBox.Text))
+                {
+
+                    sum += m.Concentration;
+                    cont++;
+                
+                }
+
+            }
+
+            //Console.WriteLine(sum + " " + cont);
+
+            if(sum > 0)
+                CO = sum/cont;
+
+
+            //-----------------------------------------------------------
+            
+            dataManager.Measurements.Clear();
+            dictionay.Clear();
+
+            dictionay.Add("Nombre del municipio", citiesComboBox.Text);
+            dictionay.Add("Variable", "O3");
+
+            dataManager.filterDataForAir(dictionay);
+            sum = 0;
+            cont = 0;
+
+            foreach (Measurement m in dataManager.Measurements)
+            {
+
+                if (m.Date.Contains(yearComboBox.Text))
+                {
+
+                    sum += m.Concentration;
+                    cont++;
+
+                }
+
+            }
+            //Console.WriteLine(sum + " " + cont);
+            if (sum > 0)
+                O3 = sum / cont;
+
+            //-----------------------------------------------------------
+
+            dataManager.Measurements.Clear();
+            dictionay.Clear();
+
+            dictionay.Add("Nombre del municipio", citiesComboBox.Text);
+            dictionay.Add("Variable", "NO2");
+
+            dataManager.filterDataForAir(dictionay);
+            sum = 0;
+            cont = 0;
+
+            foreach (Measurement m in dataManager.Measurements)
+            {
+
+                if (m.Date.Contains(yearComboBox.Text))
+                {
+
+                    sum += m.Concentration;
+                    cont++;
+
+                }
+
+            }
+            //Console.WriteLine(sum + " " + cont);
+            if (sum > 0)
+                NO2 = sum / cont;
+
+            //-----------------------------------------------------------
+
+            dataManager.Measurements.Clear();
+            dictionay.Clear();
+
+            dictionay.Add("Nombre del municipio", citiesComboBox.Text);
+            dictionay.Add("Variable", "SO2");
+
+            dataManager.filterDataForAir(dictionay);
+            sum = 0;
+            cont = 0;
+
+            foreach (Measurement m in dataManager.Measurements)
+            {
+
+                if (m.Date.Contains(yearComboBox.Text))
+                {
+
+                    sum += m.Concentration;
+                    cont++;
+
+                }
+
+            }
+            //Console.WriteLine(sum + " " + cont);
+            if (sum > 0)
+                SO2 = sum / cont;
+
+            //-----------------------------------------------------------
+
+            dataManager.Measurements.Clear();
+            dictionay.Clear();
+
+            dictionay.Add("Nombre del municipio", citiesComboBox.Text);
+            dictionay.Add("Variable", "PM10");
+
+            dataManager.filterDataForAir(dictionay);
+            sum = 0;
+            cont = 0;
+
+            foreach (Measurement m in dataManager.Measurements)
+            {
+
+                if (m.Date.Contains(yearComboBox.Text))
+                {
+
+                    sum += m.Concentration;
+                    cont++;
+
+                }
+
+            }
+            //Console.WriteLine(sum + " " + cont);
+            if (sum > 0)
+                PM10 = sum / cont;
+
+            //-----------------------------------------------------------
+
+            dataManager.Measurements.Clear();
+            dictionay.Clear();
+
+            dictionay.Add("Nombre del municipio", citiesComboBox.Text);
+            dictionay.Add("Variable", "PM2.5");
+
+            dataManager.filterDataForAir(dictionay);
+            sum = 0;
+            cont = 0;
+
+            foreach (Measurement m in dataManager.Measurements)
+            {
+
+                if (m.Date.Contains(yearComboBox.Text))
+                {
+
+                    sum += m.Concentration;
+                    cont++;
+
+                }
+
+            }
+            //Console.WriteLine(sum + " " + cont);
+            if (sum > 0)
+                PM25 = sum / cont;
+
+            //Console.WriteLine("listo");
+
+        }
+
+    }
+}
