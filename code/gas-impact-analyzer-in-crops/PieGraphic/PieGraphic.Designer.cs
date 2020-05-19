2a95a949c72a3f41183824c85fc1717b138e1b54
Felipe Sánchez <vegafelipe015@gmail.com>
2020-05-18 19:56:32 -0500

    Pie graphic ready

--- code/gas-impact-analyzer-in-crops/PieGraphic/PieGraphic.Designer.cs
+++ code/gas-impact-analyzer-in-crops/PieGraphic/PieGraphic.Designer.cs
@@ -29,21 +29,69 @@
         private void InitializeComponent()
         {
             this.pieHarvested = new LiveCharts.WinForms.PieChart();
+            this.citiesComboBox = new System.Windows.Forms.ComboBox();
+            this.yearComboBox = new System.Windows.Forms.ComboBox();
+            this.ShowButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // pieHarvested
             // 
-            this.pieHarvested.Location = new System.Drawing.Point(188, 12);
+            this.pieHarvested.Location = new System.Drawing.Point(157, 41);
             this.pieHarvested.Name = "pieHarvested";
-            this.pieHarvested.Size = new System.Drawing.Size(358, 181);
+            this.pieHarvested.Size = new System.Drawing.Size(467, 263);
             this.pieHarvested.TabIndex = 1;
             this.pieHarvested.Text = "Harvested";
+            // 
+            // citiesComboBox
+            // 
+            this.citiesComboBox.FormattingEnabled = true;
+            this.citiesComboBox.Items.AddRange(new object[] {
+            "Cali",
+            "Palmira",
+            "Candelaria",
+            "Buga",
+            "Buenaventura"});
+            this.citiesComboBox.Location = new System.Drawing.Point(287, 336);
+            this.citiesComboBox.Name = "citiesComboBox";
+            this.citiesComboBox.Size = new System.Drawing.Size(121, 21);
+            this.citiesComboBox.TabIndex = 2;
+            this.citiesComboBox.Text = "Cali";
+            // 
+            // yearComboBox
+            // 
+            this.yearComboBox.FormattingEnabled = true;
+            this.yearComboBox.Items.AddRange(new object[] {
+            "2011",
+            "2012",
+            "2013",
+            "2014",
+            "2015",
+            "2016",
+            "2017"});
+            this.yearComboBox.Location = new System.Drawing.Point(287, 379);
+            this.yearComboBox.Name = "yearComboBox";
+            this.yearComboBox.Size = new System.Drawing.Size(121, 21);
+            this.yearComboBox.TabIndex = 3;
+            this.yearComboBox.Text = "2011";
+            // 
+            // ShowButton
+            // 
+            this.ShowButton.Location = new System.Drawing.Point(522, 336);
+            this.ShowButton.Name = "ShowButton";
+            this.ShowButton.Size = new System.Drawing.Size(75, 23);
+            this.ShowButton.TabIndex = 4;
+            this.ShowButton.Text = "Show";
+            this.ShowButton.UseVisualStyleBackColor = true;
+            this.ShowButton.Click += new System.EventHandler(this.ShowButton_Click);
             // 
             // PieGraphic
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ShowButton);
+            this.Controls.Add(this.yearComboBox);
+            this.Controls.Add(this.citiesComboBox);
             this.Controls.Add(this.pieHarvested);
             this.Name = "PieGraphic";
             this.Text = "Pie Graphic";
@@ -53,6 +101,9 @@
 
         #endregion
         private LiveCharts.WinForms.PieChart pieHarvested;
+        private System.Windows.Forms.ComboBox citiesComboBox;
+        private System.Windows.Forms.ComboBox yearComboBox;
+        private System.Windows.Forms.Button ShowButton;
     }
 }
 
--- code/gas-impact-analyzer-in-crops/PieGraphic/PieGraphic.cs
+++ code/gas-impact-analyzer-in-crops/PieGraphic/PieGraphic.cs
@@ -7,14 +7,94 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using model;
 
 namespace PieGraphic
 {
     public partial class PieGraphic : Form
     {
+        private DataManager dm;
+
         public PieGraphic()
         {
             InitializeComponent();
+            dm = new DataManager();
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            pieHarvested.Series.Clear();
+
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+            dictionary.Add("Municipios", citiesComboBox.Text);
+            dictionary.Add("AÑO", yearComboBox.Text);
+            
+
+            dm.filterDataForHarvested(dictionary);
+            showData();
+            
+            
+        }
+
+        private void showData() 
+        {
+
+            List<string> list = supportList();
+
+            SeriesCollection sc = new SeriesCollection();
+            PieSeries ps;
+
+            CropMeasurement cm = dm.Harvested[0];
+
+            foreach (string st in list)
+            {
+
+                ps = new PieSeries()
+                {
+
+                    Title = st,
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(cm.getTypeCrop(st)) },
+                    DataLabels = true,
+
+                };
+
+                sc.Add(ps);
+            }
+
+            pieHarvested.Series = sc;
+            
+        
+        }
+
+        private List<string> supportList() 
+        {
+
+            List<string> list = new List<string>();
+
+            list.Add("AlgodonA");
+            list.Add("AlgodonB");
+            list.Add("ArrozA");
+            list.Add("ArrozB");
+            list.Add("FrijolA");
+            list.Add("FrijolB");
+            list.Add("MaizTecnificadoA");
+            list.Add("MaizTecnificadoB");
+            list.Add("SorgoA");
+            list.Add("SorgoB");
+            list.Add("SoyaA");
+            list.Add("SoyaB");
+            list.Add("TabacoA");
+            list.Add("TabacoB");
+            list.Add("MaizTradicionalA");
+            list.Add("MaizTradicionalB");
+
+
+            return list;
+        
         }
     }
 }
