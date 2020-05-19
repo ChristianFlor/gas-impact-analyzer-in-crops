890f436bc8bf2378ef457bf08e13a4500ed7d0f1
Daniel Fernandez <alejo.dafr@gmail.com>
2020-05-18 22:54:54 -0500

    Fixed query for kmeans

--- code/gas-impact-analyzer-in-crops/model/DataManager.cs
+++ code/gas-impact-analyzer-in-crops/model/DataManager.cs
@@ -29,6 +29,35 @@ namespace model
             measurements = new List<Measurement>(1400000);
             harvested = new List<CropMeasurement>(1400000);
             planted = new List<CropMeasurement>(1400000);
+
+            List<string> list = new List<String>
+            {
+                "Nombre del municipio"
+            };
+            readInfo(airQualityRepository, airQualityId);
+            string[] values = new string[1];
+            values[0] = "BUENAVENTURA"; query2(list, values, "measurements");
+            values[0] = "GUADALAJARA%20DE%20BUGA"; query2(list, values, "measurements");
+            values[0] = "CALI"; query2(list, values, "measurements");
+            values[0] = "CANDELARIA"; query2(list, values, "measurements");
+            values[0] = "JAMUNDÍ"; query2(list, values, "measurements");
+            values[0] = "PALMIRA"; query2(list, values, "measurements");
+            values[0] = "YUMBO"; query2(list, values, "measurements");
+
+            readInfo(harvestRepository, harvestId);
+            list = new List<String>(); list.Add("Municipios");
+            values[0] = "Cali"; query2(list, values, "harvested");
+            values[0] = "Palmira"; query2(list, values, "harvested");
+            values[0] = "Candelaria"; query2(list, values, "harvested");
+            values[0] = "Buga"; query2(list, values, "harvested");
+            values[0] = "Buenaventura"; query2(list, values, "harvested");
+            readInfo(plantedRepository, plantedId);
+            list = new List<String>(); list.Add("Municipios");
+            values[0] = "Cali"; query2(list, values, "planted");
+            values[0] = "Palmira"; query2(list, values, "planted");
+            values[0] = "Candelaria"; query2(list, values, "planted");
+            values[0] = "Buga"; query2(list, values, "planted");
+            values[0] = "Buenaventura"; query2(list, values, "planted");
         }
 
         public void initializeKmeans(string crop)
@@ -309,6 +338,90 @@ namespace model
             query(parametros, valores2, "planted");
 
 
+        }
+
+
+
+
+
+
+        private void query2(List<string> list, string[] values, string type)
+        {
+            string url2 = url;
+            string str;
+            int year = -1;
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (!list[i].Contains("$"))
+                {
+                    str = columnDeserialize[list[i]] + "=" + values[i] + "&";
+                    url2 += str;
+                }
+                else
+                {
+                    year = i;
+                }
+            }
+            if (year > -1)
+            {
+                url2 += list[year] + "=" + values[year];
+            }
+            string rawData = new WebClient().DownloadString(url2);
+            Console.WriteLine(url2);
+            if (type.Equals("measurements"))
+            {
+                string[] regs = rawData.Substring(1, rawData.Length - 2).Split('}');
+                foreach (string r in regs)
+                {
+                    string s = r.Replace("\n", "").Replace("\"", "");
+                    if (s.Length > 10)
+                    {
+                        try
+                        {
+                            string[] attrs = s.Substring(1).Split(',');
+                            string date = attrs[0].Substring(7);
+                            string authority = attrs[1].Split(':')[1];
+                            string stationName = attrs[2].Split(':')[1];
+                            string technology = attrs[3].Split(':')[1];
+                            double latitude = ParseDouble(attrs[4].Split(':')[1]);
+                            double longitude = ParseDouble(attrs[5].Split(':')[1]); ;
+                            string departmentCode = attrs[6].Split(':')[1];
+                            string department = attrs[7].Split(':')[1];
+                            string municipalityCode = attrs[8].Split(':')[1];
+                            string municipality = attrs[9].Split(':')[1];
+                            string stationType = attrs[10].Split(':')[1];
+                            double exhibitionTime = ParseDouble(attrs[11].Split(':')[1]);
+                            string variable = attrs[12].Split(':')[1];
+                            string unit = attrs[13].Split(':')[1];
+                            double concentration = ParseDouble(attrs[14].Split(':')[1]);
+                            Measurement m = new Measurement(date, authority, stationName, technology, latitude, longitude, departmentCode, department, municipalityCode, municipality, stationType, exhibitionTime, variable, unit, concentration);
+                            measurements.Add(m);
+                        }
+                        catch { 
+                            
+                        }
+                    }
+                }
+                Console.WriteLine("Measurements has " + measurements.Count() + " elements");
+            }
+            else if (type.Equals("harvested"))
+            {
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                CropMeasurement[] data = js.Deserialize<CropMeasurement[]>(rawData);
+                foreach (CropMeasurement m in data)
+                {
+                    harvested.Add(m);
+                }
+            }
+            else
+            {
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                CropMeasurement[] data = js.Deserialize<CropMeasurement[]>(rawData);
+                foreach (CropMeasurement m in data)
+                {
+                    planted.Add(m);
+                }
+            }
         }
     }
 }
